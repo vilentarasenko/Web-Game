5fb4b669e52bb088aa8f1d27231893cfdc24d374
vilentarasenko <125544744+vilentarasenko@users.noreply.github.com>
2023-05-16 15:06:38 +0300

    script.js

--- script.js
+++ script.js
@@ -1,81 +1,49 @@
-// весь скрипт — это одна большая функция
 (function(){
 	
-	//  объявляем объект, внутри которого будет происходить основная механика игры
 	var Memory = {
 
 		// создаём карточку
 		init: function(cards){
-			//  получаем доступ к классам
 			this.$game = $(".game");
 			this.$modal = $(".modal");
 			this.$overlay = $(".modal-overlay");
 			this.$restartButton = $("button.restart");
-			// собираем из карточек массив — игровое поле
+			
 			this.cardsArray = $.merge(cards, cards);
-			// перемешиваем карточки
 			this.shuffleCards(this.cardsArray);
-			// и раскладываем их
 			this.setup();
 		},
-
-		// как перемешиваются карточки
 		shuffleCards: function(cardsArray){
 			// используем встроенный метод .shuffle
 			this.$cards = $(this.shuffle(this.cardsArray));
 		},
 
-		// раскладываем карты
 		setup: function(){
-			// подготавливаем код с карточками на страницу
 			this.html = this.buildHTML();
-			// добавляем код в блок с игрой
 			this.$game.html(this.html);
-			// получаем доступ к сформированным карточкам
 			this.$memoryCards = $(".card");
-			// на старте мы не ждём переворота второй карточки
 			this.paused = false;
-			// на старте у нас нет перевёрнутой первой карточки
      		this.guess = null;
-     		// добавляем элементам на странице реакции на нажатия
 			this.binding();
 		},
-
-		// как элементы будут реагировать на нажатия
 		binding: function(){
-			// обрабатываем нажатие на карточку
 			this.$memoryCards.on("click", this.cardClicked);
-			// и нажатие на кнопку перезапуска игры
 			this.$restartButton.on("click", $.proxy(this.reset, this));
 		},
-
-		// что происходит при нажатии на карточку
 		cardClicked: function(){
 			// получаем текущее состояние родительской переменной
 			var _ = Memory;
-			// и получаем доступ к карточке, на которую нажали
 			var $card = $(this);
-			// если карточка уже не перевёрнута и мы не нажимаем на ту же самую карточку второй раз подряд
 			if(!_.paused && !$card.find(".inside").hasClass("matched") && !$card.find(".inside").hasClass("picked")){
-				// переворачиваем её
 				$card.find(".inside").addClass("picked");
-				// если мы перевернули первую карточку
 				if(!_.guess){
-					// то пока просто запоминаем её
 					_.guess = $(this).attr("data-id");
-				// если мы перевернули вторую и она совпадает с первой
 				} else if(_.guess == $(this).attr("data-id") && !$(this).hasClass("picked")){
-					// оставляем обе на поле перевёрнутыми и показываем анимацию совпадения
 					$(".picked").addClass("matched");
-					// обнуляем первую карточку
 					_.guess = null;
-						// если вторая не совпадает с первой
 						} else {
-							// обнуляем первую карточку
 							_.guess = null;
-							// не ждём переворота второй карточки
 							_.paused = true;
-							// ждём полсекунды и переворачиваем всё обратно
 							setTimeout(function(){
 								$(".picked").removeClass("picked");
 								Memory.paused = false;
@@ -83,7 +51,6 @@
 						}
 				// если мы перевернули все карточки
 				if($(".matched").length == $(".card").length){
-					// показываем победное сообщение
 					_.win();
 				}
 			}
@@ -91,23 +58,18 @@
 
 		// показываем победное сообщение
 		win: function(){
-			// не ждём переворота карточек
 			this.paused = true;
-			// плавно показываем модальное окно с предложением сыграть ещё
 			setTimeout(function(){
 				Memory.showModal();
 				Memory.$game.fadeOut();
 			}, 1000);
 		},
 
-		// показываем модальное окно
 		showModal: function(){
-			// плавно делаем блок с сообщением видимым
 			this.$overlay.show();
 			this.$modal.fadeIn("slow");
 		},
 
-		// прячем модальное окно
 		hideModal: function(){
 			this.$overlay.hide();
 			this.$modal.hide();
@@ -115,13 +77,9 @@
 
 		// перезапуск игры
 		reset: function(){
-			// прячем модальное окно с поздравлением
 			this.hideModal();
-			// перемешиваем карточки
 			this.shuffleCards(this.cardsArray);
-			// раскладываем их на поле
 			this.setup();
-			// показываем игровое поле
 			this.$game.show("slow");
 		},
 
@@ -138,13 +96,9 @@
 		    return array;
 		},
 
-		// код, как добавляются карточки на страницу
 		buildHTML: function(){
-			// сюда будем складывать HTML-код
 			var frag = '';
-			// перебираем все карточки подряд
 			this.$cards.each(function(k, v){
-				// добавляем HTML-код для очередной карточки
 				frag += '<div class="card" data-id="'+ v.id +'"><div class="inside">\
 				<div class="front"><img src="'+ v.img +'"\
 				alt="'+ v.name +'" /></div>\
@@ -152,7 +106,6 @@
 				alt="Codepen" /></div></div>\
 				</div>';
 			});
-			// возвращаем собранный код
 			return frag;
 		}
 	};
@@ -224,7 +177,6 @@
 		},
 	];
     
-	// запускаем игру
 	Memory.init(cards);
 
 
